32bc06fa896aa2dfeb4891311e6599bc46e4356b
wony1114 <wony1114@nate.com>
2020-12-23 18:16:42 +0900

    2020-12-23

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -43,18 +43,18 @@ public class HomeController {
         return String.format("%s/%s", dir, page);
     }
 
-    @GetMapping("/transfer/{dir}/{sub}/{page}")
-    public String transfer(@PathVariable String dir, @PathVariable String sub, @PathVariable String page) {
+    @GetMapping("/admin/{dir}/{page}")
+    public String admin(@PathVariable String dir, 
+    						@PathVariable String page) {
         logger.info("이동 디렉토리: " + dir);
-        logger.info("이동 서브 디렉토리: " + sub);
         logger.info("이동 파일: " + page);
-        return String.format("%s/%s/%s", dir, sub, page);
+        return String.format("admin:%s/%s", dir, page);
     }
-    
     @GetMapping("/home")
     public String home() {
-    	 logger.info("타일즈 테스트 진입 성공");
+    	logger.info("타일즈 테스트 진입 성공" );
     	return "public";
     }
+  
 	
 }
